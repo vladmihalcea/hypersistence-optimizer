b6ce088e1185e9233e3123e8d8ed4dd43e8d4fb0
sivaprasadreddy <sivaprasadreddy.k@gmail.com>
2020-12-12 09:04:57 +0530

    Upgrading SpringBoot and JUnit versions in SpringBoot example

--- hypersistence-optimizer-spring-boot-example/src/main/java/io/hypersistence/optimizer/forum/Application.java
+++ hypersistence-optimizer-spring-boot-example/src/main/java/io/hypersistence/optimizer/forum/Application.java
@@ -2,13 +2,11 @@ package io.hypersistence.optimizer.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Vlad Mihalcea
  */
 @SpringBootApplication
-@ComponentScan(basePackages = "io.hypersistence.optimizer.forum")
 public class Application {
 
     public static void main(String[] args) {
