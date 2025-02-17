eb953c2a968fa2c28202bce7998c21f6e8a4245f
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2025-02-17 15:51:13 +0200

    Bump Hypersistence Optimizer version to 2.11.0

--- test.bat
+++ test.bat
@@ -1,5 +1,8 @@
 @echo off
 
-mvn clean install %*
+call mvn -pl hypersistence-optimizer-test-case,hypersistence-optimizer-config-example,hypersistence-optimizer-spring-hibernate-example,hypersistence-optimizer-spring-hibernate4-example,hypersistence-optimizer-spring-hibernate3-example,hypersistence-optimizer-spring-jpa-example,hypersistence-optimizer-spring-jpa-hibernate4-example,hypersistence-optimizer-spring-jpa-hibernate3-example,hypersistence-optimizer-micronaut-example clean install %* || exit /b  
+set JAVA_HOME=%JAVA_HOME_17%
+call mvn -pl hypersistence-optimizer-spring-boot3-example,hypersistence-optimizer-spring-boot-example,hypersistence-optimizer-spring-boot-jta-example,hypersistence-optimizer-glassfish-hibernate-example,hypersistence-optimizer-glassfish-hibernate4-example,hypersistence-optimizer-quarkus-spring-example,hypersistence-optimizer-quarkus-example,hypersistence-optimizer-micronaut2-example clean install %* || exit /b
 
 goto:eof
+
