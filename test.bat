90f60cdf683f10d92925bbefea2a8ee7c782ef8e
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2025-10-17 16:56:04 +0300

    Break test suite into three parts

--- test.bat
+++ test.bat
@@ -1,8 +1,7 @@
 @echo off
 
-call mvn -pl hypersistence-optimizer-test-case,hypersistence-optimizer-config-example,hypersistence-optimizer-spring-hibernate-example,hypersistence-optimizer-spring-hibernate4-example,hypersistence-optimizer-spring-hibernate3-example,hypersistence-optimizer-spring-jpa-example,hypersistence-optimizer-spring-jpa-hibernate4-example,hypersistence-optimizer-spring-jpa-hibernate3-example,hypersistence-optimizer-micronaut-example clean install %* || exit /b  
-set JAVA_HOME=%JAVA_HOME_17%
-call mvn -pl hypersistence-optimizer-spring-boot3-example,hypersistence-optimizer-spring-boot-example,hypersistence-optimizer-spring-boot-jta-example,hypersistence-optimizer-glassfish-hibernate-example,hypersistence-optimizer-glassfish-hibernate4-example,hypersistence-optimizer-quarkus-spring-example,hypersistence-optimizer-quarkus-example,hypersistence-optimizer-micronaut2-example clean install %* || exit /b
+call test1 || exit /b
+call test2 || exit /b
+call test3 || exit /b
 
 goto:eof
-
